2c9dff814532dfcb0574120de8c3d34ce86e37d5
Sapporun <sapporu@gmail.com>
2017-01-24 13:27:08 +0100

    uppdatering
    test

---
+++ GRUPP1/GRUPP1/src/grupp1/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupp1;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Sapporun
+ */
+public class Main {
+
+    public static void main(String[] args) {
+            Database.getDB();
+            Login login1 = new Login();
+            login1.setVisible(true);
+            login1.setLocationRelativeTo(null);
+    }
+
+}
