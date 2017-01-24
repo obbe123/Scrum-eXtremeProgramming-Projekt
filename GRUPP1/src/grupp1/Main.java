3abd366f546f2f1ed62a27f17c365d1d94c392a9
Nötfärs <Ntfrs@10.22.12.12>
2017-01-24 11:55:19 +0100

    ändrat

--- GRUPP1/src/grupp1/Main.java
+++ GRUPP1/src/grupp1/Main.java
@@ -18,6 +18,9 @@ public class Main {
 
     public static void main(String[] args) {
             Database.getDB();
+            Login login1 = new Login();
+            login1.setVisible(true);
+            login1.setLocationRelativeTo(null);
     }
 
 }
