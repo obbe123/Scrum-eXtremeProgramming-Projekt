bdf5b89e7087a71d7524a7c3cbc43048666fa8da
Gittarn <erik.axner@gmail.com>
2017-01-25 15:47:51 +0100

    ey
    boro

--- GRUPP15/src/grupp1/Person.java
+++ GRUPP15/src/grupp1/Person.java
@@ -1,12 +1,47 @@
 
+
 package grupp1;
 
 
 public class Person {
     
+    private String fNamn;
+    private String eNamn;
+    private int telefon;
+    private String admin;
     private String email;
+    private String losenord;
     
-    public Person(String ey){
-        email = ey;
+    public Person(String fNamn, String eNamn, int telefon, String admin, String email, String losenord){
+        this.fNamn = fNamn;
+        this.eNamn = eNamn;
+        this.telefon = telefon;
+        this.admin = admin;
+        this.email = email;
+        this.losenord = losenord;
+    }
+    
+    public String getFNamn(){
+        return fNamn;
+    }
+    
+    public String getENamn(){
+        return eNamn;
+    }
+    
+    public int getTelefon(){
+        return telefon;
+    }
+    
+    public String getAdmin(){
+        return admin;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public String getlosenord(){
+        return losenord;
     }
 }
