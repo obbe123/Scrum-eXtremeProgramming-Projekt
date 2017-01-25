30bb8270fe87d16afda9295e33eb147e0b32916e
Gittarn <erik.axner@gmail.com>
2017-01-25 15:18:36 +0100

    hehe
    hehe

--- GRUPP15/src/grupp1/Person.java
+++ GRUPP15/src/grupp1/Person.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package grupp1;
 
-/**
- *
- * @author Nötfärs
- */
+
 public class Person {
     
+    private String email;
+    
+    public Person(String ey){
+        email = ey;
+    }
 }
