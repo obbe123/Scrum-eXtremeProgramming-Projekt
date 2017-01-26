7238af90c944d6bf43fbe8594dc81759d66b1f77
sofianacke <sofia.nacke@hotmail.com>
2017-01-26 12:19:38 +0100

    test

--- GRUPP15/src/grupp1/Database.java
+++ GRUPP15/src/grupp1/Database.java
@@ -15,7 +15,7 @@ import java.io.File;
  * @author Sapporun
  */
 public class Database {
-    public int test2;
+    public int test3;
 
     public static Connection getDB() {
         String filePath = "";
