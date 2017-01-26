dd5c7a203cde5a1c705197dfce099544bdf038f6
itsLoreena <frida.niklasson88@gmail.com>
2017-01-26 14:12:50 +0100

    test7

--- GRUPP15/src/grupp1/Database.java
+++ GRUPP15/src/grupp1/Database.java
@@ -15,7 +15,7 @@ import java.io.File;
  * @author Sapporun
  */
 public class Database {
-    public int test7;
+    public int test8;
 
     public static Connection getDB() {
         String filePath = "";
